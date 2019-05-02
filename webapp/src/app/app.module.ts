58cbd1149c170901752bb2012538dbc3ce792433
beolabs.io <beolabs.io@outlook.fr>
2019-05-02 02:02:24 +0200

    Add Carousel Component

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    CarouselComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent, NavbarComponent]
+  bootstrap: [AppComponent, NavbarComponent, CarouselComponent]
 })
 export class AppModule { }
