0fcf83253df312788ba35169a4d7953b72c404ed
beolabs.io <beolabs.io@outlook.fr>
2019-05-05 20:12:09 +0200

    Global WIP

--- webapp/src/app/components/contact/contact.component.ts
+++ webapp/src/app/components/contact/contact.component.ts
@@ -8,4 +8,41 @@ import { Component } from '@angular/core';
 
 export class ContactComponent {
   
+  title = "Horaires d'ouverture";
+
+  openingDays = [
+
+    {
+      day: 'Lundi',
+      open: '9AM',
+      close: '8PM',
+    },
+    {
+      day: 'Mardi',
+      open: '9AM',
+      close: '8PM',
+    },
+    {
+      day: 'Mercredi',
+      open: '9AM',
+      close: '8PM',
+    },
+    {
+      day: 'Jeudi',
+      open: '9AM',
+      close: '8PM',
+    },
+    {
+      day: 'Vendredi',
+      open: '9AM',
+      close: '8PM',
+    },
+    {
+      day: 'Samedi',
+      open: '9AM',
+      close: '8PM',
+    },
+
+  ]
+
 }
